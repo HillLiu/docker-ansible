33e5d62c614f41aac923d529da7e9917e61802a1
Hill <hill@kimo.com>
2023-02-10 11:51:58 +0800

    update preview

--- docs/preview.sh
+++ docs/preview.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+DIR="$(
+  cd "$(dirname "$0")"
+  pwd -P
+)"
 ENV="${DIR}/.env"
+IMAGE_NAME="hillliu/mdbook"
 
 if [ -e "${ENV}" ]; then
   MDBOOK_SRC=$(awk -F "=" '/^MDBOOK_SRC/ {print $2}' $ENV)
@@ -9,57 +13,59 @@ if [ -e "${ENV}" ]; then
     MDBOOK_SRC=$DIR/$MDBOOK_SRC
   fi
   CONTAINER_NAME=$(awk -F "=" '/^CONTAINER_NAME/ {print $2}' $ENV)
-  PORT=$(awk -F "=" '/^PORT/ {print $2}' $ENV)
+  ENV_PORT=$(awk -F "=" '/^PORT/ {print $2}' $ENV)
+  if [ ! -z "${ENV_PORT}" ]; then
+    PORT=$ENV_PORT
+  fi
 fi
 
 MDBOOK_SRC=${MDBOOK_SRC:-$DIR}
 CONTAINER_NAME=${CONTAINER_NAME:-mdbook}
-PORT=${PORT:-3888}
 
-# echo $MDBOOK_SRC
-# echo $CONTAINER_NAME
-# echo $PORT
-# exit;
-
-OpenCmd=$(which xdg-open 2>/dev/null)
+OpenCmd=$(which xdg-open 2> /dev/null)
 case "$OSTYPE" in
   linux*)
-    if [ -z "$OpenCmd" ]; then 
-      OpenCmd="echo" 
+    if [ -z "$OpenCmd" ]; then
+      OpenCmd="echo"
     fi
     ;;
   darwin*)
-    OpenCmd="open" 
+    OpenCmd="open"
     ;;
   *)
-    if [ -z "$OpenCmd" ]; then 
-      OpenCmd="echo" 
+    if [ -z "$OpenCmd" ]; then
+      OpenCmd="echo"
     fi
     ;;
 esac
 
 start() {
+  PORT=${PORT:-3888}
+  # echo $MDBOOK_SRC
+  # echo $CONTAINER_NAME
+  # echo $PORT
+  # exit;
   stop
-  cmd="docker run -p ${PORT}:${PORT}";
+  cmd="docker run -p ${PORT}:${PORT} -e PORT=${PORT} -u $(id -u):$(id -g)"
   if [ -e "${DIR}/book.toml" ]; then
-    cmd+=" -v ${DIR}/book.toml:/mdbook/book.toml";
+    cmd+=" -v ${DIR}/book.toml:/mdbook/book.toml"
   fi
-  cmd+=" -v ${MDBOOK_SRC}:/mdbook/src --name ${CONTAINER_NAME} --rm -d hillliu/mdbook serve -n 0.0.0.0 -p ${PORT}";
-  echo $cmd;
-  echo $cmd | bash
-  sleep 5 
-  echo ${OpenCmd} http://localhost:${PORT} | bash
+  cmd+=" -v ${MDBOOK_SRC}:/mdbook/src --name ${CONTAINER_NAME} --rm -d ${IMAGE_NAME} server"
+  echo $cmd
+  echo $cmd | sh
+  sleep 5
+  echo ${OpenCmd} http://localhost:${PORT} | sh
   logs
 }
 
 build() {
-  cmd="docker run";
+  cmd="docker run"
   if [ -e "${DIR}/book.toml" ]; then
-    cmd+=" -v ${DIR}/book.toml:/mdbook/book.toml";
+    cmd+=" -v ${DIR}/book.toml:/mdbook/book.toml"
   fi
-  cmd+=" -v ${MDBOOK_SRC}:/mdbook/src --rm -d hillliu/mdbook build -d /mdbook/src/docs";
-  echo $cmd;
-  echo $cmd | bash
+  cmd+=" -v ${MDBOOK_SRC}:/mdbook/src --rm -d ${IMAGE_NAME} build -d /mdbook/src/docs"
+  echo $cmd
+  echo $cmd | sh
 }
 
 stop() {
@@ -67,8 +73,8 @@ stop() {
   if [ "x$res" == "x$CONTAINER_NAME" ]; then
     docker stop ${CONTAINER_NAME}
   fi
-#  docker stop ${CONTAINER_NAME}
-#  docker rm ${CONTAINER_NAME}
+  #  docker stop ${CONTAINER_NAME}
+  #  docker rm ${CONTAINER_NAME}
 }
 
 status() {
@@ -77,6 +83,10 @@ status() {
 
 logs() {
   docker logs -f ${CONTAINER_NAME}
+}
+
+pull() {
+  docker pull ${IMAGE_NAME}
 }
 
 case "$1" in
@@ -95,9 +105,17 @@ case "$1" in
   logs)
     logs
     ;;
+  pull)
+    pull
+    ;;
   *)
-    echo "$0 [start|stop|build|status|logs]"
+    binPath=$0
+    if [ "$binPath" == "bash" ] || [ "$binPath" == "sh" ]; then
+      binPath="curl https://raw.githubusercontent.com/HillLiu/docker-mdbook/main/bin/preview.sh | bash -s --"
+    fi
+    echo "$binPath [start|stop|build|status|logs|pull]"
     exit
+    ;;
 esac
 
 exit $?
