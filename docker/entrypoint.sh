6ff68ed04ff2633c394e1f57c1d897e7fa64b401
Hill <hill@kimo.com>
2023-06-23 15:03:31 +0800

    sync kye and sshconfig with vpn

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,13 +7,19 @@ server() {
 
 if [ -e "/key" ]; then
   eval $(ssh-agent -a /usr/local/share/ssh-auth-sock)
-  ssh-add /key
+  for key in /key*; do
+    ssh-add $key
+  done
 fi
 
-if [ -e "/sshconfig" ]; then
-  mkdir -p /root/.ssh/
-  cp /sshconfig /root/.ssh/config
-fi
+# init /root/.ssh/config
+mkdir -p /root/.ssh/
+for sshconfig in /sshconfig*; do
+  if [ -f "$sshconfig" ]; then
+    cat $sshconfig >> /root/.ssh/config
+    echo "" >> /root/.ssh/config
+  fi
+done
 
 if [ "$1" = 'server' ]; then
   server
